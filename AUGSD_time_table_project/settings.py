95610f089b5ef1f8dbff958d762638b594ec0875
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-03-25 02:31:44 +0530

    add django-heroku

--- AUGSD_time_table_project/settings.py
+++ AUGSD_time_table_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = '/course-load/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+django_heroku.settings(locals())
