830d60cb1fa35b71b58350042723d61c7a805744
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-03-25 01:45:30 +0530

    try4

--- AUGSD_time_table_project/settings.py
+++ AUGSD_time_table_project/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'client/build/static'),
+    # os.path.join(BASE_DIR, 'client/build/static'),
 ]
 
 LOGIN_REDIRECT_URL = '/course-load/dashboard/'
