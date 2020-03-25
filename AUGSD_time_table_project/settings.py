462d1a05a25ee8986f363f186f4bf93b7aa18319
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-03-26 03:34:39 +0530

    Add whitenoise middleware

--- AUGSD_time_table_project/settings.py
+++ AUGSD_time_table_project/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AUGSD_time_table_project.urls'
@@ -125,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/course-load/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
